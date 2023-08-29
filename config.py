ac6939ead1e1ad9ad532b915f198cfad480e51b3
jasonliew777 <143489548+jasonliew777@users.noreply.github.com>
2023-08-29 15:42:05 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cey6fvqyqrdu.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "liewjunxian-employee"
+customregion = "us-east-1"
+
